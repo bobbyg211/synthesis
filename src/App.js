78612e8f608db345393c9ce47765fdc1b529bade
bobbyg211 <robertdg96@gmail.com>
2021-05-15 18:05:53 -0400

    Initial commit
    Contains the skeleton of a React app with a working Express server and some test user data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./styles.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("/users.json").then((users) => setUsers(users.data));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello users!</h1>
+      <ul className="users">
+        {users.map((user) => (
+          <li className="user">
+            <p>
+              <strong>Name:</strong> {user.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>City:</strong> {user.address.city}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
