a2203583f5e6d09e58d6976beabf136ff356bc48
bobbyg211 <robertdg96@gmail.com>
2021-06-24 16:19:46 -0400

    Dynamic redirect URI

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
+  const redirect = process.env.REACT_APP_REDIRECT_URI;
   const { loginWithRedirect } = useAuth0();
   return (
     <button
       className="btn btn-primary btn-block"
       onClick={() =>
         loginWithRedirect({
-          redirectUri: "http://localhost:3000/journals",
+          redirectUri: redirect,
         })
       }
     >
