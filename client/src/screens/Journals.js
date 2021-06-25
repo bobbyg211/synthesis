7d36de3c7d98d67e405dec12eb8f57a3995446d5
bobbyg211 <robertdg96@gmail.com>
2021-06-24 21:59:10 -0400

    Create user fix

--- client/src/screens/Journals.js
+++ client/src/screens/Journals.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Typography } from "@material-ui/core";
 import CreateIcon from "@material-ui/icons/Create";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -7,12 +7,30 @@ import AddJournal from "../components/AddJournal";
 import useJournals from "../hooks/useJournals";
 import useDeleteJournal from "../hooks/useDeleteJournal";
 import { useAuth0 } from "@auth0/auth0-react";
+import useCreateUser from "../../src/hooks/useCreateUser";
 
 export default function Journals() {
   const { user } = useAuth0();
   const [journalId, setJournalId] = useState();
   const { isLoading, isError, data } = useJournals({ user_id: user.sub });
   const deleteJournal = useDeleteJournal({ id: journalId });
+
+  let user_data;
+  if (user) {
+    user_data = {
+      id: user.sub,
+      first_name: user.given_name,
+      last_name: user.family_name,
+    };
+  }
+
+  const createUser = useCreateUser(user_data);
+
+  useEffect(() => {
+    if (user) {
+      createUser.mutate();
+    }
+  }, []);
 
   if (isLoading || isError) {
     return null;
